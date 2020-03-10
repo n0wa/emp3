09a768217eb41546d3b70a5305dd0dc1288adc01
NK <nawang.kawasaki@cunamutual.com>
2020-03-10 07:23:13 -0500

    First commit

---
+++ emp.py
@@ -0,0 +1,74 @@
+#!flask/bin/python
+from flask import Flask, jsonify
+from flask import abort
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow
+
+#SQL query
+import pyodbc
+import sqlalchemy as sal
+from sqlalchemy import create_engine
+import pandas as pd
+#---------------------------------
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://nk:nK1234@52.179.195.78:1433/test?driver=SQL+Server+Native+Client+11.0"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init ma
+ma = Marshmallow(app)
+
+
+
+# Product Class/Model
+class Emp(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(100))
+  zip = db.Column(db.String(5))
+  
+ 
+# Employee Schema
+class  EmpSchema(ma.Schema):
+  class Meta:
+    fields = ('id', 'name', 'zip')
+
+# Init schema
+emp_schema = EmpSchema()
+emps_schema = EmpSchema(many=True)
+
+# Get All Employees
+@app.route('/emp', methods=['GET'])
+def get_emps():
+  all_emps = Emp.query.all()
+  result = emps_schema.dump(all_emps)
+  return jsonify(result)
+
+	
+# Get Single Employee
+@app.route('/emp/<id>', methods=['GET'])
+def get_emp(id):
+  emp = Emp.query.get(id)
+  return emp_schema.jsonify(emp)
+  
+# Run Server
+if __name__ == '__main__':
+  app.run(debug=True)
+
+  
+#--------------------------------------
+#SQL Query
+#sal.create_engine('dialect+driver://nk:nK1234@52.179.195.78:1433/test')
+#sal.create_engine('mssql+pyodbc://nk:nK1234@52.179.195.78:1433/test?driver=ODBC+Driver+17+for+SQL+Server')
+#engine = sal.create_engine("mssql+pyodbc://nk:nK1234@52.179.195.78:1433/test?driver=SQL+Server+Native+Client+11.0")
+
+
+# establishing the connection to the databse using engine as an interface
+#conn = engine.connect()
+
+#conn.close()
+#--------------------------------------------
+
+
