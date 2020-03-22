d5416e07a09560a1e325a6cb11c59bbf533b4784
NK <nawang.kawasaki@cunamutual.com>
2020-03-22 11:41:02 -0500

    Key Vault & Service Principal

--- app.py
+++ app.py
@@ -18,9 +18,35 @@ import pyodbc
 import pandas as pd
 #-------------------------------------------
 
+from azure.keyvault import KeyVaultClient, KeyVaultAuthentication
+from azure.common.credentials import ServicePrincipalCredentials
+
+#----------------------------
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://nk:nK1234@52.179.161.106:1433/test?driver=ODBC Driver 17 for SQL Server"#11.0
+
+VAULT_URL = 'https://devcu-kv.vault.azure.net/'  #'https://devcu-kv.vault.azure.net/'
+USR_SECRET = 'usr'
+PASS_SECRET = 'passwd'
+USR_SECRET_VERSION = '0bdad91564384ad0a459ad5ba5c941c5'
+PASS_SECRET_VERSION = 'd7c1186833da4612bb7e2707b53306d2'
+
+credentials = ServicePrincipalCredentials(
+    client_id = '0dafa328-0f67-40cb-97bc-8b38b28931af',
+    secret = 'licTySGcAk16Ly8IuK/v-1Uc?oamNBR/',
+    tenant = '76b869e6-f3cf-4e3e-b85e-2a0e58ad9b63'
+)
+
+client = KeyVaultClient(credentials)
+
+Usr = client.get_secret(VAULT_URL, USR_SECRET, USR_SECRET_VERSION ).value
+Pass = client.get_secret(VAULT_URL, PASS_SECRET, PASS_SECRET_VERSION ).value
+
+
+#print('Username: ' +Usr +'\n Password: ' +Pass)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://nk:nK1234@52.179.161.106:1433/test?driver=ODBC Driver 17 for SQL Server"#11.0
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc://"+ Usr+ ":" +Pass + "@52.179.161.106:1433/test?driver=ODBC Driver 17 for SQL Server"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init db
